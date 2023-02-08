4c9622a3914a0f82152ca3b8f8c9eeafc5eed74a
LizaShu <lizashu0904@gmail.com>
2023-02-08 15:24:43 +0300

    feat: task brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let count = 0;
+  for (let i = 0; i < str.lengt; i++) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
+      count++;
+    }
+    if (str[i] === ')' || str[i] === ']' || str[i] === '}') {
+      count--;
+  }
+  if (count < 0) {
+    return false;
+  }
+}
+if (count != 0) {
+  return false
+} else {return true}
 }
